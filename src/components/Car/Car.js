6287f128435399317fc798defd404a81de75123e
Dillan <dillanroby@yahoo.com>
2020-08-24 02:59:57 -0700

    added Car and Cars to components, both still need css files completed

---
+++ src/components/Car/Car.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Car.css';
+
+function Car (props) {
+    const { car, list } = props;
+
+    return (
+        <Link to={`/cars/${car._id}`}>
+            <div className="car-card">
+              <section className="image-wrapper">
+                  <img src={car.CarPicUrl} alt={car.make, car.model} height="100" />
+              </section>
+              <section className="content-wrapper">
+                <h4>{car.make, car.model}</h4>
+              </section>
+            </div>
+        </Link>
+    );
+}
+
+export default Car;
---
+++ src/components/Car/Cars/Cars.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Car from '../Car/Car';
+
+import '/Cars.css';
+
+function Cars(props) {
+    const cars = props.cars.map((carObj) => {
+        return <Car key={carObj._id} car={carObj} list={true} />
+    });
+
+    return (
+        <div className="cars-container">
+            {cars}
+        </div>
+    );
+}
+
+export default Cars;
