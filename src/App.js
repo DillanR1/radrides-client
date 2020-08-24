802772a786b4761ae9dadb875f1ee6aba78a0078
Dillan <dillanroby@yahoo.com>
2020-08-24 01:40:50 -0700

    added code to app, routes, index and models for Car. added pages folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
+import Routes from './config/routes';
 import './App.css';
 
 function App() {
---
+++ src/config/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Home from '../pages/Home/Home';
+import CarsContainer from '../pages/CarsContainer/CarsContainer';
+import CarContainer from '../pages/CarsContainer/CarsContainer';
+import NewCarContainer from '../pages/NewCarContainer/NewCarContainer';
+
+// Log in and Reg could be stretch goals
+
+export default() => (
+    <Switch>
+        <Route exact path='/' component={Home} />
+
+        <Route path='/cars/new' render={() =>
+         <NewCarContainer />
+        
+        }/>
+ 
+        <Route path='/cars/:id' component={CarContainer} />
+        <Route path='/cars' component={CarsContainer} />
+   </Switch>
+);
+
+//NOTE: Some of the unread Aliases and values are intentional for future expansions and stretch goals
