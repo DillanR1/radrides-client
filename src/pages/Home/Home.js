7583c7321f3bf731f2f67f0c1b9753cc83ac32d9
Dillan <dillanroby@yahoo.com>
2020-09-29 13:07:05 -0700

    Broken links fixed, minor tweaks

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 
-
-
 const Home = () => {
     return (
 
   <div id="homePic">
     <h1 style={{color:'#13b0cc'}}>Rad Rides</h1>
-    <img src={'https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-0/p180x540/117445408_3197235657029717_1929237182400864772_o.jpg?_nc_cat=102&_nc_sid=8bfeb9&_nc_ohc=eqOcq-WJUj0AX_p3XCB&_nc_ht=scontent-sjc3-1.xx&tp=6&oh=fd461b770aa2c026deb15d17ce6b568d&oe=5F710F2D'} alt=''></img>
+    <img src={'https://static.carthrottle.com/workspace/uploads/posts/2017/01/74abbdd13384b259073e6cb57327343b.jpg'} alt=''></img>
    
   </div>
   );
